96bd795dab76c77032819c8053b792aee66a0527
Oliver Adameck <oliveradameck@Olivers-MacBook-Pro-2.local>
2019-05-27 12:33:33 +0200

    Implementation of chor-js

--- src/components/ModelView.vue
+++ src/components/ModelView.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="modelview">
+    <div id="canvas"></div>
     <h1>{{ message }}</h1>
     <p>Here, the Chor JS needs to be implemented</p>
+    <div class="floating-button">
+      Deploy
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ChoreoModeler from 'chor-js';
+
+const modeler = new ChoreoModeler({
+  container: '#canvas',
+});
 
 @Component
 export default class ModelView extends Vue {
@@ -16,22 +25,24 @@ export default class ModelView extends Vue {
     };
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #canvas {
+    height: 400px;
+  }
+  .floating-button {
+    position: fixed;
+    bottom: 40px;
+    right: 40px;
+    background-color: #75b900;
+    padding: 10px;
+    border-radius: 40px;
+    font-size: 1em;
+    color: #ffffff;
+    font-weight: bold;
+    cursor: pointer;
+    box-shadow: -4px 4px 4px #3e3f42;
+  }
 </style>
