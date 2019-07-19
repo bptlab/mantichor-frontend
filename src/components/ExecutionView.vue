53add6a88b6a64e636916054dfdb2fb12dd922cf
Oliver Adameck <oliver.adameck@gmail.com>
2019-07-19 12:58:27 +0200

    Button to publish a note

--- src/components/ExecutionView.vue
+++ src/components/ExecutionView.vue
@@ -9,6 +9,7 @@
         <p>Project Name:<br>{{ $projectmanagement.activeProject.name }}</p>
         <p>Element ID:<br>{{ elementID }}</p>
         <p>Task:<br>{{ elementTask }}</p>
+        <button title="publish on Blockchain" @click="publish()">Publish</button>
       </div>
       <div v-else>
         <p>Please select an element.</p>
@@ -70,6 +71,10 @@ export default class ExecutionView extends Vue {
 
   private resetZoom() {
     this.modeler.get('canvas').zoom('fit-viewport');
+  }
+
+  private publish() {
+    // Backend request ...
   }
 
   private mounted() {
